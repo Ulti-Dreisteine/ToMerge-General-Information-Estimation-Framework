bb0c8f97f2eb20534ad8565c2024d0be0f01c999
Dreisteine-Matebook16 <dreisteine262@163.com>
2023-06-03 21:17:08 +0800

    update: 更新统计效能检测结果

--- script/独立性检验/statistical_power_test.py
+++ script/独立性检验/statistical_power_test.py
@@ -68,33 +68,66 @@ if __name__ == "__main__":
     alpha = 0.01
     rounds = 100
 
-    # Ns = [50, 100, 200, 500, 1000]
-    # scales = [0.0, 0.1, 0.2, 0.5, 1.0, 2.0]
-    Ns = [50, 60]
-    scales = [0.0, 0.1]
-
+    Ns = [50, 100, 200, 500, 1000]
+    scales = [0.0, 0.1, 0.2, 0.5, 1.0, 2.0]
+    
     funcs = ["random", "line", "parabola", "circular", "sin_high_freq", "exp_base_2"]
-    fig = plt.figure()
-    for i, func in enumerate(funcs):
-        results = defaultdict(dict)
-        for N, scale in itertools.product(Ns, scales):
-            print(f"computing {N}, {scale} ...")
-            _rate = cal_assoc_rate(func, N, scale)
-            results[N][scale] = _rate  
-        results_df = pd.DataFrame.from_dict(results)
+
+    # ---- 各数据关系统计效力测试 --------------------------------------------------------------------
+    
+    recal_rates = False
+    if recal_rates:
+        for i, func in enumerate(funcs):
+            print(f"\ndataset: {func}")
+            results = defaultdict(dict)
+            for N, scale in itertools.product(Ns, scales):
+                print(f"computing {N}, {scale} ...")
+                _rate = cal_assoc_rate(func, N, scale)
+                results[N][scale] = _rate  
+            results_df = pd.DataFrame.from_dict(results)
+            
+            # 保存结果至本地
+            np.save(f"file/{func}_rate_results.npy", results_df.values)
         
-        # 绘制曲面图
-        ax = fig.add_subplot(3, 2, i + 1, projection="3d")
-        Ns_grid, scales_grid = np.meshgrid(Ns, scales)
+    # 绘制曲面图
+    Ns_grid, scales_grid = np.meshgrid(Ns, scales)
+    
+    fig = plt.figure(figsize=(16, 8))
+    for i, func in enumerate(funcs):
+        values = np.load(f"file/{func}_rate_results.npy")
+        
+        ax = fig.add_subplot(2, 3, i + 1, projection="3d")
         ax.plot_surface(
             scales_grid, 
             Ns_grid, 
-            results_df.values,
-            # cmap=cm.coolwarm,
+            values,
+            cmap=cm.viridis,
             linewidth=0,
-            antialiased=False)
-        ax.set_title(f"dataset: {func}")
-        ax.set_xlabel("sigma")
-        ax.set_ylabel("N")
-        ax.set_zlabel("rate")
+            antialiased=True)
+        # ax.set_title(f"dataset: {func}", fontsize=16)
+        ax.set_xlabel("noise scale $\sigma$", fontsize=14)
+        ax.set_ylabel("sample size $N$", fontsize=14)
+        ax.set_zlabel("assoc rate detected", fontsize=14)
+        ax.text(0, 0, np.max(values) * 1.5 - 0.5 * np.min(values), func, fontsize=16)
+        # ax.set_zlim([-0.1, 1.1])
     plt.tight_layout()
+    plt.savefig("img/统计效能测试结果.png", dpi=450)
+    
+    # ---- 绘制各数据关系分布 ------------------------------------------------------------------------
+    
+    for func in funcs:
+        x, y = gen_test_data(func, N=2000, scale=0.1)
+        # if func == "exp_base_2":
+        #     fs=(1.4, 1.2)
+        # else:
+        #     fs=(1.2, 1.2)
+        plt.figure(figsize=(1.2, 1.2))
+        plt.scatter(x, y, s=3, alpha=1, c="k", lw=0.1, facecolor="w")
+        plt.xticks([], [])
+        plt.yticks([], [])
+        # plt.xlabel("variable $x$", fontsize=14)
+        # plt.ylabel("variable $y$", fontsize=14)
+        # plt.ticklabel_format(style="sci", scilimits=(-2, 3), axis="y")
+        # plt.tight_layout()
+        # plt.show()
+        plt.savefig(f"img/data_{func}.png", dpi=450)
