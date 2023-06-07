059a287eac7d0c2c5c3e5cc41f3a9455d99e6824
DreisteineSCU <dreisteine262@163.com>
2023-06-07 15:19:52 +0800

    add: 加入一致性测试代码

---
+++ script/一致性测试/setting.py
@@ -0,0 +1,60 @@
+NS = [200, 500, 1000, 2000, 3000]
+# NS = [500, 1000]
+
+FUNC_NAMES = [
+    # "circular",
+    "sin_high_freq",
+    "cos_high_freq",
+    "vary_freq_sin",
+    "vary_freq_cos",
+    "non_fourier_freq_sin",
+    "non_fourier_freq_cos",
+    "sin_low_freq",
+    "linear_periodic_med_freq",
+    "cubic",
+    "cubic_y_stretched",
+    "parabola",
+    "spike",
+    "lopsided_l_shaped",
+    "l_shaped",
+    "sigmoid",
+    "exp_base_10",
+    "exp_base_2",
+    "linear_periodic_high_freq_2",
+    "linear_periodic_high_freq",
+    "linear_periodic_low_freq",
+    "line",
+]
+
+# AMPS = [
+#     0.01, 0.1, 0.3, 0.5, 0.7, 0.9, 1.15, 1.45, 1.8, 2.5, 4.5, 8.0, 10.0, 20.0
+# ]
+AMPS = [
+    0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.15, 1.3, 1.45, 1.6, 1.8, 2.1, 2.5,
+    3.1, 4.5, 6.0, 8.0, 10.0, 20.0
+    ]
+
+FUNC_NAMES_ALIAS = [
+    # "circular",
+    "sin high freq", 
+    "cos high freq",
+    "vary freq sin", 
+    "vary freq cos", 
+    "non fourier freq sin",
+    "non fourier freq cos", 
+    "sin low freq", 
+    "linear periodic med freq",
+    "cubic", 
+    "cubic y stretched", 
+    "parabola", 
+    "spike",
+    "lopsided l shaped", 
+    "l shaped", 
+    "sigmoid",
+    "exp base 10",
+    "exp base 2",
+    "linear periodic high freq 2", 
+    "linear periodic high freq",
+    "linear periodic low freq",
+    "line",
+]
